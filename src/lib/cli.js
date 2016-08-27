6e0e9c5770feac54479903683c6f30bf7429ee90
Stefan Thomas <justmoon@members.fsf.org>
2016-08-27 12:12:10 -0700

    feat(gfx): prettier console output and verify before executing
    Print nice colorful console output.
    
    Also adds a feature to allow users to abort before applying
    any changes.

--- src/lib/cli.js
+++ src/lib/cli.js
@@ -3,13 +3,18 @@
 const co = require('co')
 const fetch = require('node-fetch')
 const tv4 = require('tv4')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
 const Promise = require('bluebird')
-const merge = require('lodash/merge')
+const semverRegex = require('semver-regex')
+const mergeWith = require('lodash/mergeWith')
 const mapValues = require('lodash/mapValues')
 const zipObject = require('lodash/zipObject')
 const omit = require('lodash/omit')
 const isUndefined = require('lodash/isUndefined')
 const isFunction = require('lodash/isFunction')
+
+const gfx = require('./gfx')
 
 const PLUGIN_DEFAULTS = {
   hooks: {},
@@ -24,20 +29,44 @@ class Cli {
   }
 
   * _run (argv) {
-    console.log('Loading template')
-    const template = yield this.loadTemplate(argv)
-    console.log('Loading plugins')
-    let plugins = yield this.loadPlugins(template)
-    console.log('Loading hooks')
-    const hooks = yield this.getHooks(plugins)
-    console.log('Validating state')
+    gfx.renderTruck()
+    gfx.logStepHeadline('Loading policy')
+    const policy = yield this.loadTemplate(argv)
+    let plugins = yield this.loadPlugins(policy)
+    const hooks = this.getHooks(plugins)
+    gfx.logStepHeadline('Validating state')
     plugins = yield this.validateState(plugins, hooks)
-    if (!argv.validateOnly) {
-      console.log('Predicting actions')
+    if (plugins.length && !argv.validateOnly) {
+      gfx.logStepHeadline('Predicting actions')
       yield this.predictActions(plugins, hooks)
-      console.log('Executing actions')
-      yield this.executeActions(plugins, hooks)
+      console.info()
+      const answers = yield inquirer.prompt([{
+        type: 'confirm',
+        name: 'confirmed',
+        message: 'Would you like to execute these actions?',
+        default: false
+      }])
+
+      if (answers.confirmed) {
+        gfx.logStepHeadline('Executing actions')
+        yield this.executeActions(plugins, hooks)
+
+        console.info()
+        console.info(chalk.green.bold('All done'))
+      } else {
+        console.info()
+        console.info(chalk.yellow.bold('Aborted by user'))
+      }
+    } else if (!plugins.length) {
+      console.info()
+      console.info(chalk.green.bold('Everything valid'))
+    } else {
+      console.warn()
+      console.warn(chalk.yellow.bold('Repository is not compliant, exiting with status 1'))
+      console.warn()
+      process.exit(1)
     }
+    console.info()
   }
 
   loadTemplate (argv) {
@@ -45,15 +74,37 @@ class Cli {
   }
 
   * _loadTemplate (argv) {
-    const templateUri = argv._[0]
+    const policyUri = argv._[0]
 
-    const templateResult = yield fetch(templateUri)
-    const template = yield templateResult.json()
+    const policyResult = yield fetch(policyUri)
+    const policy = yield policyResult.json()
 
-    // Validate repotool template
-    tv4.validate(template, require('../../schemas/Template.json'))
+    // Validate repotool policy
+    const validation = tv4.validateResult(policy, require('../../schemas/Policy.json'))
+    if (!validation.valid) {
+      console.error()
+      console.error(chalk.red(' invalid') + ' ' +
+        chalk.bold('Policy did not match the schema'))
+      console.error()
+      console.error('         at ' + validation.error.schemaPath + ': ' +
+        validation.error.message)
+      console.error()
+      process.exit(1)
+    }
 
-    this.template = template
+    if (!semverRegex().test(policy.version)) {
+      console.error()
+      console.error(chalk.red(' invalid') + ' ' +
+        chalk.bold('Policy contains invalid version: ' + chalk.red(policy.version)))
+      console.error()
+      process.exit(1)
+    }
+
+    console.info()
+    console.info(chalk.green(' success') + ' Using policy ' +
+      chalk.bold(policy.name + '@' + policy.version))
+
+    this.policy = policy
   }
 
   loadPlugins () {
@@ -62,7 +113,7 @@ class Cli {
 
   * _loadPlugins () {
     const missingPlugins = []
-    const plugins = this.template.plugins.map((config) => {
+    const plugins = this.policy.plugins.map((config) => {
       const moduleName = 'repotool-plugin-' + config.module
       try {
         // We only resolve the plugin, because if we required it here it might
@@ -78,12 +129,20 @@ class Cli {
 
       const plugin = require(moduleName)
 
-      return Object.assign({ module: config.module }, PLUGIN_DEFAULTS, plugin(config))
+      return Object.assign(
+        {},
+        PLUGIN_DEFAULTS,
+        plugin(config),
+        { module: config.module }
+      )
     })
 
     if (missingPlugins.length) {
-      console.log('Missing plugins, please run:')
-      console.log('npm install -g ' + missingPlugins.join(' '))
+      console.error()
+      console.error(chalk.red(' plugins') + ' Missing plugins, please run:')
+      console.error()
+      console.error('         npm install -g ' + missingPlugins.join(' '))
+      console.error()
       process.exit(1)
     }
 
@@ -127,17 +186,31 @@ class Cli {
   validateState (plugins, hooks) {
     // Run validation on all plugins and return plugin objects that had negative
     // results during validation
+    console.info()
     return Promise.filter(plugins, (plugin) => {
-      return Promise.resolve(plugin.validate(hooks)).then(val => !val)
-    })
+      return Promise.resolve(plugin.validate(hooks))
+        .then(result => {
+          console.info(chalk.grey('      =>') + ' ' + plugin.module + ' ' +
+            (result ? chalk.green.bold('✓') : chalk.yellow.bold('needs changes')))
+          return !result
+        })
+    }, { concurrency: 1 })
   }
 
   predictActions (plugins, hooks) {
-    return Promise.all(plugins.map((plugin) => plugin.predict(hooks)))
+    console.info()
+    return Promise.all(plugins.map((plugin) => {
+      console.info(chalk.grey('--------') + ' ' + plugin.module + ':')
+      plugin.predict(hooks)
+    }))
   }
 
   executeActions (plugins, hooks) {
-    return Promise.all(plugins.map((plugin) => plugin.execute(hooks)))
+    console.info()
+    return Promise.all(plugins.map((plugin) => {
+      console.info(chalk.grey('--------') + ' ' + plugin.module + ':')
+      return plugin.execute(hooks)
+    }))
   }
 }
 
